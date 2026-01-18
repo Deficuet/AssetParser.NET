ba6e3ca09f0df126711fd6de0e81cb7c9c79ee87
Deficuet <Deficuet457@gmail.com>
2026-01-18 12:49:14 -0700

    refactor function for building type tree

--- TypeTreeUtils/TypeTreeHelper.cs
+++ TypeTreeUtils/TypeTreeHelper.cs
@@ -2,21 +2,26 @@
 
 public static class TypeTreeHelper
 {
+    public static void AddNode(this Stack<TypeTreeNode> stack, TypeTreeNode node)
+    {
+        while (stack.Count > 0 && stack.Peek().level >= node.level)
+        {
+            stack.Pop();
+        }
+        if (stack.Count > 0)
+        {
+            stack.Peek().children.Add(node);
+        }
+        stack.Push(node);
+    }
+
     public static TypeTreeNode BuildTypeTree(List<TypeTreeNode> nodes)
     {
         TypeTreeNode rootNode = nodes[0];
         Stack<TypeTreeNode> stack = new();
         foreach (var node in nodes)
         {
-            while (stack.Count > 0 && stack.Peek().level >= node.level)
-            {
-                stack.Pop();
-            }
-            if (stack.Count > 0)
-            {
-                stack.Peek().children.Add(node);
-            }
-            stack.Push(node);
+            stack.AddNode(node);
         }
         stack.Clear();
         return rootNode;
