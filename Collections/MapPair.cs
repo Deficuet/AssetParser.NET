10561e9aa13229776814c3cd7baf71acd0c51fee
Deficuet <Deficuet457@gmail.com>
2026-01-27 12:09:40 -0700

    make MapPair implement IEquatable

--- Collections/MapPair.cs
+++ Collections/MapPair.cs
@@ -4,7 +4,7 @@ using Serde;
 namespace AssetParser.Collections;
 
 [SerdeTypeOptions(Proxy = typeof(MapPairProxy))]
-public readonly struct MapPair<TFirst, TSecond>(TFirst first, TSecond second)
+public readonly struct MapPair<TFirst, TSecond>(TFirst first, TSecond second) : IEquatable<MapPair<TFirst, TSecond>>
 {
     public readonly TFirst first = first;
     public readonly TSecond second = second;
@@ -13,5 +13,31 @@ public readonly struct MapPair<TFirst, TSecond>(TFirst first, TSecond second)
     {
         first = this.first;
         second = this.second;
+    }
+
+    public bool Equals(MapPair<TFirst, TSecond> other)
+    {
+        return EqualityComparer<TFirst>.Default.Equals(first, other.first) &&
+               EqualityComparer<TSecond>.Default.Equals(second, other.second);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is MapPair<TFirst, TSecond> pair && Equals(pair);
+    }
+
+    public static bool operator ==(MapPair<TFirst, TSecond> left, MapPair<TFirst, TSecond> right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(MapPair<TFirst, TSecond> left, MapPair<TFirst, TSecond> right)
+    {
+        return !(left == right);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(first, second);
     }
 }
